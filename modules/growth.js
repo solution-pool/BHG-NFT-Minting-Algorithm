bd3e94c96ae29eef451dbd499499069a4090aa72
supGambler <artur.agababian2020@gmail.com>
2021-09-25 23:11:15 +0300

    add spawn_url function

--- modules/growth.js
+++ modules/growth.js
@@ -8,14 +8,15 @@ function spawn (df, d, limit) {
 
     const rndLength = rnd.length
     for(i = 0; i < rndLength ; i ++) {
-        if(rnd[i] > limit) {
+        if(rnd[i] > limit || !rnd[i]) {
             continue
         }
 
-        rndmask.push(rnd[i])
+        rndmask.push(i)
     }
-
-    for(let i = 0 ; i < rndmask.length; i ++) {
+    let rndmaskLength = rndmask.length
+    
+    for(let i = 0 ; i < rndmaskLength; i ++) {
         let l = df.get_edge_length(rndmask[i])
 
         if(l < d) {
@@ -23,9 +24,42 @@ function spawn (df, d, limit) {
         }
 
         try {
-            df.split_edge(i)
+            df.split_edge(rndmask[i])
         } 
         catch(e) {
+            continue
+        }
+    }
+}
+
+function spawn_curl(df, limit, prob_spawn=1.0) {
+    let edgeNumber = df.get_enum()
+    let ind_curv = []
+    let tot_curv = []
+    let max_curv = -100000
+
+    for(let e = 0; e < edgeNumber; e ++) {
+        try {
+            let t = df.get_edge_curvature(e)
+            ind_curv[e] = t
+            tot_curv += t
+            max_curv = Math.max(max_curv, t)
+        } catch (e) {
+        }
+        let ne = ind_curv.length
+
+        for(let i = 0; i < ne; i ++) {
+            let r = Math.random()
+            let e = i
+            let t = ind_curv[e]
+
+            if(r < t / max_curv * prob_spawn) {
+                try {
+                    df.split_edge(e, minimum_length = limit)
+                } catch (e) {
+                    
+                }
+            }
         }
     }
 }
