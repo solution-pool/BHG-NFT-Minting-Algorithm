231adf6b34f16675d538f1286b614af6e0f1631b
J-Gambler <antonovserg943@gmail.com>
2021-10-01 22:07:30 +0300

    unusual growth type

---
+++ corals/Unusal/config_unusual_1803.js
@@ -0,0 +1,18 @@
+const REALSIZE = 500
+
+const NMAX  = 10 ** 5
+const ONE   = 1 / REALSIZE
+
+const STP   = ONE
+const NEARL = 3 * ONE
+const FARL  = 25 * ONE
+
+const INIT_RAD  = 6 * ONE
+const INIT_NUM  = 4
+const MID   = 0.5
+
+const FRONT = color[3]
+const BACK  = [255, 255, 255]
+let index_number = 1803
+
+
---
+++ corals/Unusal/unusual.js
@@ -0,0 +1,75 @@
+let step_count = 7
+let SIZE = parseInt(REALSIZE / 7 * step_count)
+
+const TWOPI     = 2* Math.PI
+let DF = []
+let render = []
+let np_coords = []
+let np_vert_coords = []
+let growth_flag = true
+
+Math.seedrandom(index_number)
+for(let i = 0; i < NMAX; i ++) {
+  np_coords.push([0,0,0,0])
+}
+for(let i = 0; i < NMAX; i ++) {
+  np_vert_coords.push([0,0])
+}
+
+function setup() {
+  createCanvas(REALSIZE, REALSIZE)
+  background(BACK)
+  DF = new DifferentialLine(NMAX, FARL * 2, NEARL, FARL)
+
+  let angles = []
+  for(let i = 0; i < INIT_NUM ; i ++ ) {
+    angles.push(Math.random() * TWOPI)
+  }
+  angles.sort()
+  let xys = []
+  let lock_edges
+  for (let i = 0; i < angles.length; i ++) {
+    let a = angles[i]
+    let x = 0.5 + cos(a)*0.2
+    let y = 0.5 + sin(a)*0.2
+    xys.push([x,y])
+  }
+  xys.sort()
+
+  DF.init_line_segment(xys, lock_edges=1)
+
+  render = new Render(SIZE, BACK, FRONT, wrap)
+}
+
+function draw() {
+  wrap(render)
+}
+
+function wrap (render) {
+  let res     = steps(DF)
+  let num     = DF.np_get_edges_coordinates(np_coords)
+  const real  = np_coords.slice(0, num)
+  render.clear_canvas()
+  for(let i = 0; i < real.length; i ++) {
+    let point = real[i]
+    let x1 = point[0]
+    let y1 = point[1]
+    let x2 = point[2]
+    let y2 = point[3]
+
+    let r = render.pix
+    render.circles(x1, y1, x2, y2, r)
+  }
+  return res
+}
+
+function steps(df) {
+  df.optimize_position(STP)
+  spawn(df, NEARL, 0.5)
+
+  if(df.safe_vertex_positions(3 * STP) < 0) {
+    throw new Error("Vertices reached boundary. Stopping.")
+    // setup()
+  }
+}
+  
