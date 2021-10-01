9f468f0ae77e27898cca1cc104f16cdfa426cb4c
J-Gambler <antonovserg943@gmail.com>
2021-10-01 22:03:28 +0300

    exo growth type

---
+++ corals/Exo/config_exo_58.js
@@ -0,0 +1,16 @@
+const REALSIZE = 500
+
+const NMAX  = 10 ** 5
+const ONE   = 1 / REALSIZE
+
+const STP   = ONE
+const NEARL = 4 * ONE
+const FARL  = 40 * ONE
+
+const INIT_RAD  = 6 * ONE
+const INIT_NUM  = 10
+const MID   = 0.5
+
+const FRONT = color[58]
+const BACK  = [255,255,255]
+let index_number = 58
