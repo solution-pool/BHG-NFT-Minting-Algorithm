c666f90fd332d6e3ca23747356e11f1b543756bb
J-Gambler <antonovserg943@gmail.com>
2021-10-01 22:05:29 +0300

    ultra rare growth type

---
+++ corals/Ultra/config_ultra_303.js
@@ -0,0 +1,16 @@
+const REALSIZE = 500
+
+const NMAX  = 10 ** 5
+const ONE   = 1 / REALSIZE
+
+const STP   = ONE
+const NEARL = 2 * ONE
+const FARL  = 60 * ONE
+
+const INIT_RAD  =  1 * ONE
+const INIT_NUM  = 20
+const MID   = 0.5
+
+const FRONT = color[103]
+const BACK  = [255,255,255]
+let index_number = 303
