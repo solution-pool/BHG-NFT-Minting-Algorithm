d829e10275f2ac64a9f97b6037fc159b170f2a2e
J-Gambler <antonovserg943@gmail.com>
2021-10-01 22:04:20 +0300

    normal growth type

---
+++ corals/Normal/config_normal_1506.js
@@ -0,0 +1,16 @@
+const REALSIZE = 500
+
+const NMAX  = 10 ** 5
+const ONE   = 1 / REALSIZE
+
+const STP   = ONE
+const NEARL  = 5 * ONE 
+const FARL  = 30 * ONE 
+
+const INIT_RAD  = 6 * ONE
+const INIT_NUM  = 10
+const MID   = 0.5
+
+const FRONT = color[1]
+const BACK  = [0,0,0]
+let index_number = 1506
