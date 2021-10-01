1fc8a700d1cac975595f681e5ba91684b0a26221
J-Gambler <antonovserg943@gmail.com>
2021-10-01 22:04:01 +0300

    legendary growth type

---
+++ corals/Legendary/config_legendary_166.js
@@ -0,0 +1,17 @@
+const REALSIZE = 500
+
+const NMAX  = 10 ** 5
+const ONE   = 1 / REALSIZE
+
+const STP   = ONE
+const NEARL = 6 * ONE
+const FARL  = 60 * ONE
+
+const INIT_RAD  = 6 * ONE
+const INIT_NUM  = 11
+const MID   = 0.5
+
+const FRONT = color[166]
+const BACK  = [0,0,0]
+let index_number = 166
+
