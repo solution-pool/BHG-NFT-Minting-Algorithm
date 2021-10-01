43a8c3d4c592816850b4c32acbdf614a52066e18
J-Gambler <antonovserg943@gmail.com>
2021-10-01 22:06:26 +0300

    special growth type

---
+++ corals/Special/config_special_965.js
@@ -0,0 +1,16 @@
+const REALSIZE = 500
+
+const NMAX  = 10 ** 5
+const ONE   = 1 / REALSIZE
+
+const STP   = ONE
+const NEARL = 6 * ONE
+const FARL  = 36 * ONE
+
+const INIT_RAD  = 6 * ONE
+const INIT_NUM  = 30
+const MID   = 0.5
+
+const FRONT = [0, 0, 0]
+const BACK  = color[165]
+let index_number = 965
