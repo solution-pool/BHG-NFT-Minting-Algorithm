7c06fe8beb2bc062b908d1b49720e02d0e2c061a
J-Gambler <antonovserg943@gmail.com>
2021-10-05 10:16:56 +0300

    legendary growtype config modify

--- corals/Legendary/config_legendary_196.js
+++ corals/Legendary/config_legendary_196.js
@@ -1,17 +1,22 @@
-const REALSIZE = 500
+// Those variables contain the trait of the coral.
 
-const NMAX  = 10 ** 5
-const ONE   = 1 / REALSIZE
+const SIZE          = 400       // Growth size
+const SPEED         = 1         // Growth speed
+const LOCATION      = 1         // Location
+const COLOROPTION   = 2         // Coloroption
+const RYTHM         = 2         // Growth RYTHM
 
-const STP   = ONE
-const NEARL = 6 * ONE
-const FARL  = 60 * ONE
+// Those are the config value that the algorithm uses. Do not modify this value or the coral will be changed unexpectedly.
 
-const INIT_RAD  = 6 * ONE
-const INIT_NUM  = 11
-const MID   = 0.5
-
-const FRONT = color[196]
-const BACK  = [0,0,0]
-let index_number = 196
-
+const INDEX         = 196
+const REALSIZE      = SIZE
+const NMAX          = 10 ** 4
+const ONE           = 1 / REALSIZE
+const STP           = ONE
+const NEARL         = 5 * ONE
+const FARL          = 30 * ONE
+const INIT_RAD      = 6 * ONE
+const INIT_NUM      = 10
+const MID           = 0.5
+const TWOPI         = 2 * Math.PI
+let FRONT, BACK, CURRENTSIZE
