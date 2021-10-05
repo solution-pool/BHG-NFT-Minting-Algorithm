bf732d004a72541edfef1349ee5d1bf72ef8f759
J-Gambler <antonovserg943@gmail.com>
2021-10-05 22:49:15 +0300

    exo growpath modify

--- corals/Exo/exo.js
+++ corals/Exo/exo.js
@@ -1,23 +1,81 @@
-let step_count = 7
-let SIZE = parseInt(REALSIZE / 7 * step_count)
+let np_coords, np_vert_coords, growth_flag, DF, render, coloroptions, current_front
+let step_count = 1, step_length, color_count = 0, draw_path = [], pulse_path = [], pulse_num = 50, pulse_start = false, pulse_erase = false, pulse_fill = false
 
-const TWOPI     = 2* Math.PI
-let DF = []
-let render = []
-let np_coords = []
-let np_vert_coords = []
-let growth_flag = true
-
-Math.seedrandom(index_number)
-
-for(let i = 0; i < NMAX; i ++) {
-  np_coords.push([0,0,0,0])
+function preload() {
+  let colorLength = color.length
+  coloroptions = {
+    1 : {
+      BACK : [0,0,0],
+      FRONT : color[(INDEX % colorLength)]
+    },
+    2 : {
+      BACK : [255, 255, 255],
+      FRONT : color[(INDEX % colorLength)]
+    },
+    3 : {
+      BACK : color[(INDEX % colorLength)],
+      FRONT : [0, 0, 0]
+    },
+    4 : {
+      BACK : color[(INDEX % colorLength)],
+      FRONT : [255, 255, 255]
+    },
+    5 : {
+      BACK : [255, 255, 255],
+      FRONT : [
+        color[(INDEX % colorLength)],
+        color[((INDEX + 21) % colorLength)]
+      ]
+    },
+    6 : {
+      BACK : [0, 0, 0],
+      FRONT : [
+        color[(INDEX % colorLength)],
+        color[((INDEX + 21) % colorLength)]
+      ]
+    },
+    7 : {
+      BACK : [255, 255, 255],
+      FRONT : [
+        color[(INDEX % colorLength)],
+        color[((INDEX + 14) % colorLength)],
+        color[((INDEX + 28) % colorLength)]
+      ]
+    },
+    8 : {
+      BACK : [0, 0, 0],
+      FRONT : [
+        color[(INDEX % colorLength)],
+        color[((INDEX + 14) % colorLength)],
+        color[((INDEX + 28) % colorLength)]
+      ]
+    },
+  }
+  FRONT = coloroptions[COLOROPTION].FRONT
+  BACK  = coloroptions[COLOROPTION].BACK
 }
-for(let i = 0; i < NMAX; i ++) {
-  np_vert_coords.push([0,0])
+
+function init_current_size() {
+  step_count = 1
+  if(RYTHM > 2) {
+    CURRENTSIZE   = parseInt(REALSIZE / (RYTHM - 1))
+    step_unit   = CURRENTSIZE
+    step_length = CURRENTSIZE
+  } else {
+    step_length = step_unit = CURRENTSIZE = REALSIZE
+  }
 }
 
 function setup() {
+  init_current_size()
+  growth_flag = true
+  Math.seedrandom(INDEX)
+  if(COLOROPTION > 4) {
+    current_front = FRONT[color_count]
+    color_count = (color_count + 1) % (FRONT.length)
+  } else {
+    current_front = FRONT
+  }
   createCanvas(REALSIZE, REALSIZE)
   background(BACK)
   DF = new DifferentialLine(NMAX, FARL * 2, NEARL, FARL)
@@ -29,7 +87,7 @@ function setup() {
   angles.sort()
   DF.init_circle_segment(MID, MID, INIT_RAD, angles)
 
-  render = new Render(REALSIZE, BACK, FRONT, wrap)
+  render = new Render(REALSIZE, BACK, current_front)
 }
 
 function draw() {
@@ -37,19 +95,94 @@ function draw() {
 }
 
 function wrap (render) {
+  init_coordinates()
   let res     = steps(DF)
   let num     = DF.np_get_edges_coordinates(np_coords)
-  const real  = np_coords.slice(0, num)
+  let real  = np_coords.slice(0, num)
+  if(pulse_start) {
+    real = pulse_path.shift() 
+    if(pulse_path.length >= pulse_num) {
+      pulse_erase = true
+      pulse_fill = false
+    } else {
+      pulse_erase = false
+      pulse_fill = true
+    }
+  } else {
+    if(growth_flag) {
+      draw_path.push(real)
+    } else {
+      real = draw_path.pop()
+    }
+    if(real == undefined) {
+      setup()
+      return
+    }
+  }
   render.sandstroke(real)
   return res
 }
 
-function steps(df) {
-  df.optimize_position(STP)
-  spawn(df, NEARL, 0.03)
-
-  if(df.safe_vertex_positions(3 * STP) < 0) {
-    throw new Error("Vertices reached boundary. Stopping.")
+function init_coordinates() {
+  np_coords       = new Array()
+  np_vert_coords  = new Array()
+  for(let i = 0; i < NMAX; i ++) {
+    np_coords[i] = [0,0,0,0]
   }
+  for(let i = 0; i < NMAX; i ++) {
+    np_vert_coords[i] = [0,0,0,0]
+  }
+}
+
+function steps(df) {
+  if(pulse_start) {
+    if(pulse_path.length == 0) {
+      pulse_start = false
+      if(step_count < RYTHM - 1) {
+        step_count ++
+      } else {
+        step_count = 1
+      }
+      step_length = step_unit * step_count
+    }
+  } else {
+    if(growth_flag) {
+      df.optimize_position(STP)
+      spawn(df, NEARL, 0.03)
+    }
+    if(check_step(step_length, df)) {
+      if(step_count < RYTHM - 1) {
+        pulse_start = true
+        let drawLength = draw_path.length
+        for(let i = 1; i <= pulse_num; i ++ ) {
+          pulse_path.push(draw_path[drawLength - i])
+        }
+        for(let i = pulse_num; i > 0; i -- ) {
+          pulse_path.push(draw_path[drawLength - i])
+        }
+      } else {
+        if(RYTHM == 1) {
+          noLoop()
+        }
+        growth_flag = false
+      }
+    }
+  }
+}
+
+function check_step(step, df) {
+  let temp = CURRENTSIZE
+  CURRENTSIZE = step
+  let result = df.safe_vertex_positions(3 * STP) < 0
+  CURRENTSIZE = temp
+  return result
+}
+
+function sleep(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
 }
   
