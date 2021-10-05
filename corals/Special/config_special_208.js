e2b8c835739bcfc6d77bd2895bcedf50c46d0587
J-Gambler <antonovserg943@gmail.com>
2021-10-05 14:20:21 +0300

    specical init_num modify

--- corals/Special/config_special_208.js
+++ corals/Special/config_special_208.js
@@ -13,10 +13,10 @@ const REALSIZE      = SIZE
 const NMAX          = 10 ** 4
 const ONE           = 1 / REALSIZE
 const STP           = ONE
-const NEARL         = 5 * ONE
-const FARL          = 30 * ONE
+const NEARL         = 10 * ONE
+const FARL          = 60 * ONE
 const INIT_RAD      = 6 * ONE
-const INIT_NUM      = 10
+const INIT_NUM      = 20
 const MID           = 0.5
 const TWOPI         = 2 * Math.PI
 let FRONT, BACK, CURRENTSIZE
