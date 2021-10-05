8a7520c05604b7d63172ff871c0701f2d220af85
J-Gambler <antonovserg943@gmail.com>
2021-10-05 10:17:49 +0300

    rare growtype config modify

--- corals/Rare/config_rare_221.js
+++ corals/Rare/config_rare_221.js
@@ -1,19 +1,22 @@
-const REALSIZE = 500
+// Those variables contain the trait of the coral.
 
-const NMAX  = 10 ** 5
-const ONE   = 1 / REALSIZE
+const SIZE          = 300       // Growth size
+const SPEED         = 1         // Growth speed
+const LOCATION      = 1         // Location
+const COLOROPTION   = 1         // Coloroption
+const RYTHM         = 1         // Growth RYTHM
 
-const STP   = ONE
-const NEARL = 6 * ONE
-const FARL  = 60 * ONE
+// Those are the config value that the algorithm uses. Do not modify this value or the coral will be changed unexpectedly.
 
-const INIT_RAD  = 6 * ONE
-
-const INIT_NUM  = 6
-
-const BACK  = color[21]
-const FRONT = [255, 255, 255]
-let index_number = 221
-
-
-
+const INDEX         = 221
+const REALSIZE      = SIZE
+const NMAX          = 10 ** 4
+const ONE           = 1 / REALSIZE
+const STP           = ONE
+const NEARL         = 5 * ONE
+const FARL          = 30 * ONE
+const INIT_RAD      = 6 * ONE
+const INIT_NUM      = 10
+const MID           = 0.5
+const TWOPI         = 2 * Math.PI
+let FRONT, BACK, CURRENTSIZE
