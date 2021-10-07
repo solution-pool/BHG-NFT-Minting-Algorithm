2f47ed070059818bab8570ac823d38f0183e5597
J-Gambler <antonovserg943@gmail.com>
2021-10-08 00:05:54 +0300

    creation the ones growth types

---
+++ corals/Ones/config_ones_35.js
@@ -0,0 +1,22 @@
+// Those variables contain the trait of the coral.
+
+const SIZE          = 800       // Growth size
+const SPEED         = 1         // Growth speed
+const LOCATION      = 1         // Location
+const COLOROPTION   = 8         // Coloroption
+const RYTHM         = 3         // Growth RYTHM
+
+// Those are the config value that the algorithm uses. Do not modify this value or the coral will be changed unexpectedly.
+
+const INDEX         = 35
+const REALSIZE      = SIZE
+const NMAX          = 10 ** 6
+const ONE           = 1 / REALSIZE
+const STP           = 0.1 * ONE
+const NEARL         = 10 * ONE
+const FARL          = 100 * ONE
+const INIT_RAD      = 20     * ONE
+const INIT_NUM      = 10
+const MID           = 0.5
+const TWOPI         = 2 * Math.PI
+let FRONT, BACK, CURRENTSIZE
